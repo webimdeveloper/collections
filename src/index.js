c42bc9ed9cc84c2189a352863dfb9428d18a518a
webimdeveloper <webim.developer@gmail.com>
2025-03-21 19:10:56 +0100

    test bundle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import "./main.scss";
 function component(text) {
-    const element = document.createElement('h1');
-    element.textContent = text;
-    return element;
-  }
-  
-  document.body.prepend(component('Проект собран на Webpack2'));
+  const element = document.createElement("h1");
+  element.textContent = text;
+  return element;
+}
+
+document.body.prepend(component("Проект собран на Webpack4"));
